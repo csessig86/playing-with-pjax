98fa5d76909f59d8944e8c189642e6c1a63596bb
Chris Essig <csessig86@gmail.com>
2015-12-17 10:30:51 -0600

    Add new sub page

--- project/js/script.js
+++ project/js/script.js
@@ -1,1 +1,3 @@
+// The first parameter are the links that will open up with HTML content of the subpages
+// The second parameter is the container within the page where the content from these HTML files will be place
 $(document).pjax('.nav li a', '#pjax-container')
